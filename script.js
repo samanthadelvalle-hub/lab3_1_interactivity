ae9fe10b483ea2c01ca4c48ea672a47be5677821
Samantha DelValle <samantha.delvalle@gmail.com>
2026-04-14 21:36:29 -0400

    Corrected nav menu

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const menuToggle = document.querySelector("#menu-toggle");
-const navMenu = document.querySelector("#nav-Menu");
+const navMenu = document.querySelector("#nav-menu");
 const alertButton = document.querySelector("#alert-btn");
 
-menuToggle.addEventListener("click", function() 
-  {navMenu.classList.toggle("hidden");
+menuToggle.addEventListener("click", function() {
+  navMenu.classList.toggle("hidden");
 
-    if (navMenu.classList.contains("hidden")) {
-        menuToggle.textContent = "☰ Menu";
-    } else {
-        menuToggle.textContent = "Close Menu"; 
-    }
-    });
+  if (navMenu.classList.contains("hidden")) {
+    menuToggle.textContent = "☰ Menu";
+  } else {
+    menuToggle.textContent = "Close Menu";
+  }
+});
 
 alertButton.addEventListener("click", function() {
-    alert("Hello from JavaScript!")
+  alert("Hello from JavaScript!");
 });
