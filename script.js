89266cda7d2919ea43fd0a400516114a92a3baf9
Samantha DelValle <samantha.delvalle@gmail.com>
2026-04-14 20:11:12 -0400

    added code into files

---
+++ script.js
@@ -0,0 +1,17 @@
+// TODO: 1. Select #menu-toggle and #nav-menu using document.querySelector
+
+// TODO: 2. Add a click event listener to #menu-toggle
+// Inside the listener, toggle the "hidden" class on #nav-menu
+// Hint: element.classList.toggle("hidden")
+
+// TODO: 3. Select #alert-btn
+
+// TODO: 4. Add a click event listener that shows an alert("Hello from JavaScript!")
+
+// TODO: 5. Introduce an intentional bug (optional) and ask AI to help debug
+
+// TODO: 6. Stretch goal: Add any extra interaction
+// Example ideas:
+// - Change button text when menu is open
+// - Animate menu with CSS classes
+// - Console-log every click on the page
